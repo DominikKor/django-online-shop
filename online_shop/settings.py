7848f77de559be24c5eedd2acd66246beaa16bdb
Dominik <droko@gmx.de>
2022-04-18 21:33:11 +0200

    Added basic translation (i18n)

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 import braintree
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -63,11 +64,13 @@ INSTALLED_APPS = [
     "cart.apps.CartConfig",
     "orders.apps.OrdersConfig",
     "payment.apps.PaymentConfig",
+    "coupons.apps.CouponsConfig",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -130,7 +133,16 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGES = (
+    ("en", _("English")),
+    ("de", _("German")),
+)
+
+LANGUAGE_CODE = "en"
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, "locale/")
+)
 
 TIME_ZONE = "UTC"
 
